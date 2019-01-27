c1632edd356a7a3cabc78fd67034fe8d569571e3
Miguel Alcantara <miguel.alc@gmail.com>
2019-01-27 11:07:08 -0500

    first commit

---
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -0,0 +1,14 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+
+/**
+ * Creates a multiplication object with two randomly-generated factors
+ * between 11 and 99
+ * 
+ * @return a Multiplication object with random factors
+ *
+ */
+public interface MultiplicationService {
+	Multiplication createRandomMultiplication();
+}
